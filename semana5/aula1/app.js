2c5abf931ea221719a0c57bf66bde00ab49d8865
romeuic <casarottori@gmail.com>
2023-06-26 21:03:16 -0300

    adiciona exemplo function

--- semana5/aula1/app.js
+++ semana5/aula1/app.js
@@ -1,1 +1,16 @@
 console.log("m1s05a1");
+
+// exemplo
+// f(x, y) = x * 2 - y
+// f(2, 3) = 2 * 2 - 3
+// f(2, 3) = 4 - 3
+// f(2, 3) = 1
+
+function f(x, y) {
+  const resultado = x * 2 - y;
+  return resultado;
+}
+
+const retorno = f(2, 3);
+
+console.log(retorno);

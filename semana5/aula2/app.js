915cf4a8365de28666a633f90db2785db41dba62
romeuic <casarottori@gmail.com>
2023-06-27 19:54:36 -0300

    adiciona exemplo vetor

--- semana5/aula2/app.js
+++ semana5/aula2/app.js
@@ -1,1 +1,22 @@
 console.log("m1s05a2");
+
+const texto = "casa";
+
+console.log(texto);
+
+// índice: index, idx, i
+// inicializa um vetor
+const vetor = ["arroz", "laranja", "feijao"];
+// adiciona itens
+vetor.push("batata");
+// altera item do indice 1
+vetor[1] = "cenoura";
+// mostra todo
+console.log(vetor);
+// acessa item do indice 2
+console.log(vetor[2]);
+// mostra comprimento
+console.log(vetor.length);
+// acessa o ultimo item
+console.log(vetor[vetor.length - 1]);
+console.log(vetor.at(-2));

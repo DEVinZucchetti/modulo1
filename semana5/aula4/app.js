eafbe8c375c7e2273e24deeac952f11dd7b4ff2a
romeuic <casarottori@gmail.com>
2023-06-29 20:13:09 -0300

    adiciona exemplo forEach e destructuring

--- semana5/aula4/app.js
+++ semana5/aula4/app.js
@@ -1,7 +1,23 @@
 console.log("m1s05a4");
 
+const elementoLista = document.getElementById("lista");
+
+// inicialização de array via new
+//const vetor = new Array(3, 2);
+const vetor = [3, 2];
+
+vetor.push(6);
+//console.log(vetor);
+
+// exemplo de função dentro de objeto
+// const objeto = {};
+// objeto.teste = () => {
+//   console.log("funciona");
+// };
+// objeto.teste();
+
 // array de objetos
-const listaExemplo = [
+const pessoas = [
   {
     id: 1,
     nome: "Ada",
@@ -18,3 +34,24 @@ const listaExemplo = [
     idade: 31,
   },
 ];
+
+// exemplo for comum
+for (let i = 0; i < pessoas.length; i++) {
+  //console.log(pessoas[i].nome);
+}
+// exemplo for of
+for (let item of pessoas) {
+  //console.log(item.nome);
+}
+
+// exemplo forEach
+//pessoas.forEach(({ id, nome, idade }) => {
+// equivalente a
+pessoas.forEach((item) => {
+  const { id, nome, idade } = item;
+  const elementoItem = document.createElement("li");
+
+  elementoItem.innerHTML = `Id: ${id} Nome: ${nome} Idade:${idade}`;
+
+  elementoLista.appendChild(elementoItem);
+});

0180d8976b78b51753dc110fb3d26d845ee2b1c6
romeuic <casarottori@gmail.com>
2023-07-10 20:42:08 -0300

    adiciona exemplo seletores m1s07a1

--- semana7/aula4/app.js
+++ semana7/aula4/app.js
@@ -1,1 +1,44 @@
 console.log("m1_s07_a1");
+
+// recebe um elemento
+const listaA = document.getElementById("lista-a");
+// recebe um HTMLCollection (similar a Array)
+const itens = document.getElementsByTagName("li");
+// converte um HTMLCollection em Array
+const itensArray = [...itens];
+
+const itensEmA = listaA.getElementsByTagName("li");
+console.log({ itensEmA });
+
+// itensArray.forEach((item) => {
+//   console.log(item);
+// });
+
+// console.log(listaA, itens);
+//console.log({ listaA, itens });
+
+// recebe um único elemento
+const ListaB = document.querySelector("#lista-b");
+// recebe um NodeList (similar a Array)
+const itensListaA = document.querySelectorAll("#lista-a li");
+
+// itensListaA.forEach((item) => {
+//   console.log(item);
+// });
+
+console.log({ itensListaA });
+
+// const li = document.createElement("li");
+// li.innerHTML = "Item 4";
+
+// listaA.appendChild(li);
+//
+//listaA.removeChild(itens[1]);
+
+//itens[1].parentNode.removeChild(itens[1]);
+
+function removeItem(item) {
+  item.parentNode.removeChild(item);
+}
+
+removeItem(itens[1]);

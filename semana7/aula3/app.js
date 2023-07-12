5544a165cbcfa3c4dcae67f4d0764ad2c13c7911
romeuic <casarottori@gmail.com>
2023-07-12 19:37:41 -0300

    adiciona funcoes uteis basicas m1-s07-a3

--- semana7/aula3/app.js
+++ semana7/aula3/app.js
@@ -1,1 +1,22 @@
 console.log("m1_s07_a3");
+
+const elemLista = document.getElementById("lista-itens");
+
+const dadosLista = ["Batata", "Arroz", "Ervilha"];
+
+function criaItem(item) {
+  const li = document.createElement("li");
+  li.innerHTML = item;
+  return li;
+}
+
+function atualizaTela() {
+  elemLista.innerHTML = "";
+
+  dadosLista.forEach((item) => {
+    const elemItem = criaItem(item);
+    elemLista.appendChild(elemItem);
+  });
+}
+
+atualizaTela();

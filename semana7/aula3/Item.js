53859b120ee607fe13f9a302610af99b73c68e69
romeuic <casarottori@gmail.com>
2023-07-12 20:23:32 -0300

    adiciona item tipo tarefa

--- semana7/aula3/Item.js
+++ semana7/aula3/Item.js
@@ -1,16 +1,30 @@
 class Item {
   id;
   titulo;
+  tipo;
+  estado;
 
   constructor(item) {
-    const { id, titulo } = item;
+    const { id, titulo, tipo, estado } = item;
     this.id = id;
     this.titulo = titulo;
+    this.tipo = tipo;
+    this.estado = estado;
   }
 
   criaItem() {
     const li = document.createElement("li");
-    li.innerHTML = this.titulo;
+    li.innerHTML = `
+      <p>${this.titulo}</p>
+    `;
+
+    if (this.tipo === "tarefa") {
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.checked = this.estado;
+      li.appendChild(checkbox);
+    }
+
     return li;
   }
 }
--- semana7/aula3/dados.js
+++ semana7/aula3/dados.js
@@ -2,13 +2,18 @@ export const dadosLista = [
   {
     id: 1,
     titulo: "Batata",
+    tipo: "nota",
   },
   {
     id: 2,
     titulo: "Arroz",
+    tipo: "tarefa",
+    estado: true,
   },
   {
     id: 3,
     titulo: "Ervilha",
+    tipo: "tarefa",
+    estado: false,
   },
 ];

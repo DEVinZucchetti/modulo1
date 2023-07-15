6047856220dd3e5a1e69001959561cddea8d5718
romeuic <casarottori@gmail.com>
2023-07-14 22:00:58 -0300

    adiciona valicacao cpf exercicio1 m1-s07

--- semana7/exercicios/exercicio1/Colaborador.js
+++ semana7/exercicios/exercicio1/Colaborador.js
@@ -7,10 +7,49 @@ class Colaborador {
     this.cpf = cpf;
     this.nomeCompleto = nomeCompleto;
     this.salario = salario;
+
+    const cpfEhValido = Colaborador.testaCPF(this.cpf);
+    if (cpfEhValido) {
+      console.log("CPF válido!");
+    } else {
+      console.log("CPF inválido!");
+      throw new Error("CPF inválido!");
+    }
   }
 
   promover(porcentagem) {
     this.salario = this.salario * (porcentagem / 100 + 1);
+  }
+
+  // função para testar se o CPF é válido
+  static testaCPF(strCPFsuja) {
+    const strCPF = strCPFsuja.replaceAll(".", "").replaceAll("-", "");
+    let soma = 0;
+    if (strCPF == "00000000000") {
+      return false;
+    }
+    for (let i = 1; i <= 9; i++) {
+      soma += parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    }
+    let resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+      resto = 0;
+    }
+    if (resto != parseInt(strCPF.substring(9, 10))) {
+      return false;
+    }
+    soma = 0;
+    for (let i = 1; i <= 10; i++) {
+      soma += parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+      resto = 0;
+    }
+    if (resto != parseInt(strCPF.substring(10, 11))) {
+      return false;
+    }
+    return true;
   }
 }
 
